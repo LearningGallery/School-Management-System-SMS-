a1a71caccba2c178072749dc0323dd268d271d15
AbuTalha <abutalha3005@gmail.com>
2026-05-18 17:01:22 +0800

    Updated Backend

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,11 +1,8 @@
 terraform {
   backend "azurerm" {
-    #resource_group_name  = "rgsuiamgmtfm01"
-    #storage_account_name = "stguiamgmtfm01"
-    #container_name       = "tfstate"
-    resource_group_name  = "TerraformInfraDevOps"
-    storage_account_name = "terraforminfradevops"
-    container_name       = "terraformstatefiles"
+    resource_group_name  = "rgsuiamgmtfm01"
+    storage_account_name = "stguiamgmtfm01"
+    container_name       = "tfstate"
     key                  = "uat/devsecops.tfstate"
   }
 }
